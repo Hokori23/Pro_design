da68b2a3ae715d55d0877391a8bbf80546fbd04b
Hokori23 <hokori23@qq.com>
2021-03-09 19:01:51 +0800

    feat: post&tag model

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -2,8 +2,9 @@ import User from './User'
 import Post from './Post'
 import PostComment from './PostComment'
 import PostTag from './PostTag'
-export { User, Post, PostComment, PostTag }
-export default { User, Post, PostComment, PostTag }
+import PostTagAssociation from './PostTagAssociation'
+export { User, Post, PostComment, PostTag, PostTagAssociation }
+export default { User, Post, PostComment, PostTag, PostTagAssociation }
 
 /**
  * Post : User
@@ -23,4 +24,22 @@ PostComment.belongsTo(Post, { targetKey: 'id', foreignKey: 'uid' })
 Post.hasMany(PostComment, {
   sourceKey: 'id',
   foreignKey: 'uid',
+})
+
+/**
+ * Post : PostTag
+ * N : N
+ * @description 通过中间表建立关系
+ */
+Post.belongsToMany(PostTag, {
+  through: PostTagAssociation,
+  as: 'Tags',
+  foreignKey: 'pid',
+  otherKey: 'tid',
+})
+PostTag.belongsToMany(Post, {
+  through: PostTagAssociation,
+  as: 'Posts',
+  foreignKey: 'tid',
+  otherKey: 'pid',
 })
